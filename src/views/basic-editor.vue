05f2c0030575cff289900b510c1ef2278a32dbf2
sroxck <642261686@qq.com>
2023-10-23 17:41:46 +0800

    style: basic-editor

--- src/views/basic-editor.vue
+++ src/views/basic-editor.vue
@@ -2,26 +2,22 @@
  * @Author: sroxck
  * @Date: 2023-10-23 16:12:03
  * @LastEditors: sroxck
- * @LastEditTime: 2023-10-23 16:56:34
+ * @LastEditTime: 2023-10-23 17:41:11
  * @Description: 
 -->
 <script setup lang="ts">
 import { ref } from 'vue';
-
-const props = defineProps<{
+ defineProps<{
   value?: string,
 }>()
 
-// defineEmits(['update:modelValue','blur','focus','input','keypress','keydown'])
 const divRef = ref(null)
 defineExpose({
   divRef
 })
 </script>
 <template>
-  <div contenteditable="true" :value="value" ref="divRef"
-   
-   >
+  <div contenteditable="true" :value="value" ref="divRef">
     {{ value }}
   </div>
 </template>
